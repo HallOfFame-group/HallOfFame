a21ba840d8656a3376a51b73d61df1a6b6063d37
Robin_BoyangZhou <z2367570158@gmail.com>
2018-02-28 05:24:41 -0800

    doing the main fighting machanics

---
+++ Assets/Scripts/Fighting/StaticValue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticValue : MonoBehaviour {
+
+    public static int PositionBox_Jump = 8;
+    public static int PositionBox_Ground = 9;
+}
