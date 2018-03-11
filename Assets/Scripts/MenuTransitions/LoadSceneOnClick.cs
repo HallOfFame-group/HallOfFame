96a6070480b9cfef71edcdfeb7a904ba00085fdb
supersky1201 <1003000644@qq.com>
2018-03-10 18:57:43 -0800

    StartMenu

---
+++ Assets/Scripts/MenuTransitions/LoadSceneOnClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+    //public void LoadByIndex(int sceneIndex)
+     public void LoadByClick(string level)
+    {
+        //SceneManager.LoadScene (sceneIndex);
+        SceneManager.LoadScene(level);
+    }
+}
