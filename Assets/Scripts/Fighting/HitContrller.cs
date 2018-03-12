4fa158cd62760f6c260544bc93be4bc4b1ea333b
Robin_BoyangZhou <z2367570158@gmail.com>
2018-03-12 14:56:34 -0700

    fixed a hit bug and for multiplayer controller

--- Assets/Scripts/Fighting/HitContrller.cs
+++ Assets/Scripts/Fighting/HitContrller.cs
@@ -1,8 +1,91 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(PlayerController))]
 public class HitContrller : MonoBehaviour {
 
+    private float hitTimer;
+    //private int hitFramer;
+    private PlayerController playerController;
+    private Animator animator;
 
+    public int hits = 0;
+
+    public Text test;
+
+    private string[] Attack_Name = { "Punch1", "Punch2", "Punch3", "Kick1", "Kick2", "Head Smash", "Duck Punch", "Duck Kick", "Jump Punch", "Jump Punch", "Jump Kick" };
+    private float[] Attack_HitTime = { 1f, 1f, 1f, 1f, 1f,1f,1f,1f,1f,1f};
+
+
+    private void Awake()
+    {
+        hitTimer = 0.0f;
+        //hitFramer = 0;
+        playerController = GetComponent<PlayerController>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if(collision.transform.root.CompareTag("Player"))
+        {
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+            HitContrller opponentHitController = collision.transform.root.GetComponent<HitContrller>();
+            Animator opponentAnimator = collision.transform.root.GetComponentInChildren<Animator>();
+            AnimatorStateInfo opponentStateInfo = opponentAnimator.GetCurrentAnimatorStateInfo(0);
+            //TODO: still has some issue with opponentStateInfo block
+            if (collision.name.Contains("Position") && !opponentStateInfo.IsName("Block"))
+            {
+                for (int i = 0; i < Attack_Name.Length; i++)
+                {
+                    if (stateInfo.IsName(Attack_Name[i]))
+                    {
+                        opponentHitController.hits++;
+                        opponentHitController.hitTimer = Attack_HitTime[i];
+                        opponentAnimator.SetTrigger("Hit Up");
+
+                        if(opponentHitController.hits > 7)
+                        {
+                            //    RhythmCombo.instance.Register(this.GetComponent<ComboPiece>());
+                            //    RhythmCombo.instance.Display();
+                            //    RhythmCombo.instance.nodeEventCallback = OnNodeHit;
+                            //    RhythmCombo.instance.finishedEventCallback = finished;
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    private void Update()
+    {
+
+        if (hits > 1)
+        {
+            test.text = "" + hits;
+        }
+
+        if (hitTimer>0)
+        {
+            hitTimer -= Time.deltaTime;
+        }
+        else
+        {
+            hitTimer = 0.0f;
+            hits = 0;
+            test.text = "";
+        }
+        animator.SetFloat("HitTime",hitTimer);
+
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    Debug.Log(collision.collider.name);
+    //}
 }
--- Assets/Scripts/Fighting/PlayerController.cs
+++ Assets/Scripts/Fighting/PlayerController.cs
@@ -12,25 +12,41 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody2D playerRigidbody;
     private BoxCollider2D positionBox;
 
-    public bool isGrounded;
-    public bool isCrouching;
-    public bool isBlocking;
+    public int PlayerNumber = 0;
+
+    public bool IsGrounded { get; set; }
+    public bool IsCrouching { get; set; }
+    public bool IsBlocking { get; set; }
 
     void Start()
     {
         animator = GetComponentInChildren<Animator>();
         playerRigidbody = GetComponent<Rigidbody2D>();
-        positionBox = transform.Find("PositionBox").GetComponent<BoxCollider2D>();
+        positionBox = transform.Find("Animator").Find("PositionBox").GetComponent<BoxCollider2D>();
     }
 
 
     private void FixedUpdate()
     {
+
+        if (target != null&&IsGrounded)
+        {
+            if (transform.position.x < target.position.x)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            else
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+        }
+
+
         AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
 
 
 
-        float horizontal = Input.GetAxis("360Controller_LS_XAxis");
+        float horizontal = Input.GetAxis("360Controller"+ PlayerNumber + "_LS_XAxis");
         horizontal = (horizontal == 0) ? Input.GetAxis("Keyboard_Horizontal") : horizontal;
         animator.SetFloat("WalkSpeed", horizontal);
         if (stateInfo.IsName("Walk") || stateInfo.IsName("Back Walk"))
@@ -39,42 +55,42 @@ public class PlayerController : MonoBehaviour {
         }
 
 
-        float vertical = Input.GetAxis("360Controller_LS_YAxis");
+        float vertical = Input.GetAxis("360Controller"+ PlayerNumber + "_LS_YAxis");
         vertical = (vertical == 0) ? Input.GetAxis("Keyboard_Vertical") : vertical;
-        if (vertical > 0.1f && isGrounded&&(stateInfo.IsName("Idle") || stateInfo.IsName("Walk") || stateInfo.IsName("Back Walk")))
+        if (vertical > 0.1f && IsGrounded&&(stateInfo.IsName("Idle") || stateInfo.IsName("Walk") || stateInfo.IsName("Back Walk")))
         {
-            isGrounded = false;
+            IsGrounded = false;
             animator.SetBool("isGrounded", false);
             playerRigidbody.AddForce(new Vector2(horizontal * moveSpeed, jumpHight), ForceMode2D.Impulse);
         }
 
-        if (vertical < -0.1f && isGrounded)
+        if (vertical < -0.1f && IsGrounded)
         {
-            isCrouching = true;
+            IsCrouching = true;
             animator.SetBool("isCrouching", true);
         }
         else
         {
-            isCrouching = false;
+            IsCrouching = false;
             animator.SetBool("isCrouching", false);
         }
 
-        if (Input.GetButton("360Controller_Block") || Input.GetButton("Keyboard_Block"))
+        if (Input.GetButton("360Controller"+ PlayerNumber + "_Block") || Input.GetButton("Keyboard_Block"))
         {
-            isBlocking = true;
+            IsBlocking = true;
             animator.SetBool("isBlocking", true);
         }
         else
         {
-            isBlocking = false;
+            IsBlocking = false;
             animator.SetBool("isBlocking", false);
         }
         
 
-        if (Input.GetButtonDown("360Controller_Punch") || Input.GetButtonDown("Keyboard_Punch"))
+        if (Input.GetButtonDown("360Controller"+ PlayerNumber + "_Punch") || Input.GetButtonDown("Keyboard_Punch"))
             animator.SetTrigger("Punch");
 
-        if (Input.GetButtonDown("360Controller_Kick") || Input.GetButtonDown("Keyboard_Kick"))
+        if (Input.GetButtonDown("360Controller"+ PlayerNumber + "_Kick") || Input.GetButtonDown("Keyboard_Kick"))
             animator.SetTrigger("Kick");
 
 
@@ -86,23 +102,12 @@ public class PlayerController : MonoBehaviour {
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            isGrounded = true;
+            IsGrounded = true;
 
             positionBox.gameObject.layer = StaticValue.PositionBox_Ground;
 
             animator.SetBool("isGrounded", true);
 
-            if (target != null)
-            {
-                if (transform.position.x < target.position.x)
-                {
-                    transform.localScale = new Vector3(1, 1, 1);
-                }
-                else
-                {
-                    transform.localScale = new Vector3(-1, 1, 1);
-                }
-            }
 
         }
         else if(collision.gameObject.CompareTag("Player"))
@@ -118,11 +123,4 @@ public class PlayerController : MonoBehaviour {
 
 
 
-    //private IEnumerator Jump(float horizontal, int frames)
-    //{
-    //    for (int i = 0; i < frames; i++)
-    //        yield return null;
-
-    //    playerRigidbody.AddForce(new Vector2(horizontal * moveSpeed, jumpHight), ForceMode2D.Impulse);
-    //}
 }
