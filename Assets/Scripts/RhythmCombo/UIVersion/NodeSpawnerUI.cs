cd9135070b30966cefb1bb250674f0d6b43b751e
supersky1201 <32402809+supersky1201@users.noreply.github.com>
2018-07-08 16:37:35 -0700

    RhythmComboUI version in progress

---
+++ Assets/Scripts/RhythmCombo/UIVersion/NodeSpawnerUI.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public struct Node
+{
+    public NodeButton buttonId;
+    public Sprite buttonSprite;
+}
+
+public class NodeSpawnerUI : MonoBehaviour
+{
+    public delegate void OnNodeSpawnedFinished();
+    public event OnNodeSpawnedFinished EvtOnNodeSpawnedFinished;
+
+    private bool isSpawning = false;
+    private float elapsedTime = 0.0f;
+
+    [SerializeField]
+    private GameObject spawnNode;
+
+    [SerializeField]
+    private Node[] spawnNodeSetting;
+
+    private TimedNode[] preparedNodeTimeline;
+    private int spawnCount = 0;
+    private BeatlineUI beatlineUI;
+
+    private void Start()
+    {
+        beatlineUI = FindObjectOfType<BeatlineUI>();    
+    }
+
+    private void Update()
+    {
+        if (isSpawning)
+        {
+            elapsedTime += Time.deltaTime;
+
+            if (preparedNodeTimeline[spawnCount].timeStamp - 1 <= elapsedTime)
+            {
+                // Spawn a UI image at current position
+                // Change the UI image sprite depends on the buttonId
+                // Increment spawn count
+            }
+
+            // Check if spawn has finished, and calls the event
+        }
+    }
+
+    public void Prepare(TimedNode[] nodeTimeline)
+    {
+        preparedNodeTimeline = nodeTimeline;
+    }
+
+    public void Spawn()
+    {
+        isSpawning = true;
+        SpawnNode(spawnNodeSetting[0].buttonSprite);
+    }
+
+    private void SpawnNode(Sprite sprite)
+    {
+        GameObject go = Instantiate(spawnNode, this.transform);
+        go.GetComponent<Image>().sprite = sprite;
+    }
+}
---
+++ Assets/Scripts/RhythmCombo/UIVersion/RhythmComboUI.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RhythmComboUI : MonoBehaviour
+{
+    // Rhythm combo callback event
+    public delegate void OnRhythmComboCompleted();
+    public event OnRhythmComboCompleted EvtOnRhythmComboCompleted;
+
+    // Rhythm combo onhit callback event
+    public delegate void OnRhythmComboHit();
+    public event OnRhythmComboHit EvtOnRhythmComboHit;
+
+    // Rhythm combo on miss callback event
+    public delegate void OnRhythmComboMiss();
+    public event OnRhythmComboMiss EvtOnRhythmComboMiss;
+
+    private static RhythmComboUI rhythmComboUI;
+    public static RhythmComboUI instance
+    {
+        get
+        {
+            if (!rhythmComboUI)
+            {
+                rhythmComboUI = FindObjectOfType<RhythmComboUI>();
+                if (!rhythmComboUI)
+                {
+                    Debug.LogError("RhythmCombo must be attached to an UI Canvas in scene");
+                }
+                else
+                {
+                    // Initialize RhythmCombo UI version
+                    rhythmComboUI.Init();
+                }
+            }
+
+            return rhythmComboUI;
+        }
+    }
+
+    private ComboPiece preparedComboPiece;
+
+    private NodeSpawnerUI nodeSpawner;
+    //private BeatLineUI beatline;
+
+    private void Start()
+    {
+        if (rhythmComboUI && rhythmComboUI != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Init()
+    {
+        // Initalize
+        nodeSpawner = transform.GetComponentInChildren<NodeSpawnerUI>();
+    }
+
+    public void Prepare(ComboPiece comboPiece)
+    {
+        preparedComboPiece = comboPiece;
+        nodeSpawner.Prepare(preparedComboPiece.timeNodeArray);
+    }
+
+    public void Activate()
+    {
+        nodeSpawner.Spawn();
+    }
+
+    public void Stop()
+    {
+
+    }
+
+    // Animation event handler
+    public void OnAnimationComplete()
+    {
+
+    }
+}
